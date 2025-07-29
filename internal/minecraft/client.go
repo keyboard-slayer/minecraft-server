57eb57a052e832ba3e6e725802562e1c29ea7d85
keyboard-slayer <git@keyb.moe>
2025-07-29 15:33:19 +0200

    feat: added multiple protocols

--- internal/minecraft/client.go
+++ internal/minecraft/client.go
@@ -3,6 +3,7 @@ package minecraft
 import (
 	"bufio"
 	"errors"
+	"math"
 	"net"
 	"os"
 
@@ -16,29 +17,68 @@ import (
 
 	"encoding/binary"
 
+	"github.com/beito123/nbt"
 	"github.com/charmbracelet/log"
 	"github.com/google/uuid"
 	"github.com/keyboard-slayer/minecraft-server/internal/cfb8"
 )
 
+type chatMode byte
+type particulStatus byte
+
+const (
+	enabled chatMode = iota
+	commandsOnly
+	hidden
+)
+
+func (self chatMode) string() string {
+	return []string{"enabled", "commands only", "hidden"}[self]
+}
+
+const (
+	all particulStatus = iota
+	decreased
+	minimal
+)
+
+func (self particulStatus) string() string {
+	return []string{"all", "decreased", "minimal"}[self]
+}
+
+type userConfig struct {
+	locale       string
+	viewDistance int8
+	chat         chatMode
+	chatColors   bool
+	skinPart     uint8
+	isHandLeft   bool
+	textFiltered bool
+	allowListing bool
+	particul     particulStatus
+}
+
 type userInfo struct {
 	name string
 	uuid uuid.UUID
+	cfg  userConfig
 }
 
 type client struct {
-	logger *slog.Logger
-	socket net.Conn
-	reader *bufio.Reader
-	info   userInfo
-	key    *rsa.PrivateKey
-	rng    []byte
-	enc    cipher.Stream
-	dec    cipher.Stream
-	state  State
+	id       int
+	teleport int
+	logger   *slog.Logger
+	socket   net.Conn
+	reader   *bufio.Reader
+	info     userInfo
+	key      *rsa.PrivateKey
+	rng      []byte
+	enc      cipher.Stream
+	dec      cipher.Stream
+	state    State
 }
 
-func newClient(socket net.Conn) (client, error) {
+func newClient(socket net.Conn, id int) (client, error) {
 	handler := log.NewWithOptions(os.Stderr, log.Options{
 		ReportCaller: true,
 		Level:        log.DebugLevel,
@@ -56,13 +96,15 @@ func newClient(socket net.Conn) (client, error) {
 	rand.Read(rng)
 
 	return client{
-		logger: logger,
-		reader: bufio.NewReader(socket),
-		info:   userInfo{},
-		socket: socket,
-		key:    key,
-		rng:    rng,
-		state:  Handshaking,
+		id:       id,
+		teleport: 0,
+		logger:   logger,
+		reader:   bufio.NewReader(socket),
+		info:     userInfo{},
+		socket:   socket,
+		key:      key,
+		rng:      rng,
+		state:    Handshaking,
 	}, nil
 }
 
@@ -75,7 +117,12 @@ func (self client) decode(ciphertext []byte) ([]byte, error) {
 	return plain, nil
 }
 
-func (self client) send(protocol int, contents ...any) {
+func (self *client) teleportId() int {
+	self.teleport += 1
+	return self.teleport
+}
+
+func (self client) send(protocol int, contents ...any) error {
 	payload := make([]byte, 0)
 
 	for i := range contents {
@@ -85,14 +132,38 @@ func (self client) send(protocol int, contents ...any) {
 			length := writeVarInt(len(bytes))
 			payload = append(payload, length...)
 			payload = append(payload, bytes...)
+		case []string:
+			length := writeVarInt(len(v))
+			payload = append(payload, length...)
+
+			for _, s := range v {
+				bytes := []byte(s)
+				length := writeVarInt(len(bytes))
+				payload = append(payload, length...)
+				payload = append(payload, bytes...)
+			}
 		case []byte:
 			length := writeVarInt(len(v))
 			payload = append(payload, length...)
 			payload = append(payload, v...)
+		case float32:
+			bytes := make([]byte, 4)
+			binary.BigEndian.PutUint32(bytes, math.Float32bits(v))
+			payload = append(payload, bytes...)
+		case float64:
+			bytes := make([]byte, 8)
+			binary.BigEndian.PutUint64(bytes, math.Float64bits(v))
+			payload = append(payload, bytes...)
 		case int64:
 			bytes := make([]byte, 8)
 			binary.BigEndian.PutUint64(bytes, uint64(v))
 			payload = append(payload, bytes...)
+		case int32:
+			bytes := make([]byte, 4)
+			binary.BigEndian.PutUint32(bytes, uint32(v))
+			payload = append(payload, bytes...)
+		case int:
+			payload = append(payload, writeVarInt(v)...)
 		case uuid.UUID:
 			bytes := v[:]
 			payload = append(payload, bytes...)
@@ -102,7 +173,19 @@ func (self client) send(protocol int, contents ...any) {
 			} else {
 				payload = append(payload, byte(0))
 			}
+		case nbt.Tag:
+			stream := nbt.NewStream(nbt.BigEndian)
+			if err := stream.WriteTag(v); err != nil {
+				return err
+			}
+
+			bytes := stream.Bytes()
+			payload = append(payload, bytes...)
+
+		default:
+			return errors.New("Got an unknown type")
 		}
+
 	}
 
 	payloadWithProt := append(writeVarInt(protocol), payload...)
@@ -115,6 +198,8 @@ func (self client) send(protocol int, contents ...any) {
 		self.enc.XORKeyStream(cipher, final)
 		self.socket.Write(cipher)
 	}
+
+	return nil
 }
 
 func (self client) close() {
@@ -185,6 +270,7 @@ func (self *client) register(name string, id uuid.UUID) {
 	self.info = userInfo{
 		name: name,
 		uuid: id,
+		cfg:  userConfig{},
 	}
 }
 
--- internal/minecraft/protocol.go
+++ internal/minecraft/protocol.go
@@ -8,6 +8,7 @@ import (
 	"encoding/binary"
 	"encoding/json"
 
+	// "github.com/beito123/nbt"
 	"github.com/google/uuid"
 )
 
@@ -18,56 +19,206 @@ const (
 	Status
 	Login
 	Config
+	Play
 )
 
 func (self State) string() string {
-	return []string{"Handshaking", "Status", "Login", "Config"}[self]
+	return []string{"Handshaking", "Status", "Login", "Config", "Play"}[self]
 }
 
 func resName(c *client, id int) string {
-	res := [][]string{
-		{"intention", "status_request", "hello", "??"},
-		{"??", "ping_request", "key", "??"},
-		{"??", "??", "??", "custom_payload"},
-		{"??", "??", "login_acknowledged", "??"},
+	switch id {
+	case 0x00:
+		return []string{"intention", "status_request", "hello", "client_information", "??"}[c.state]
+	case 0x01:
+		return []string{"??", "ping_request", "key", "??", "??"}[c.state]
+	case 0x02:
+		return []string{"??", "??", "??", "custom_payload", "??"}[c.state]
+	case 0x03:
+		return []string{"??", "??", "login_acknowledged", "finish_configuration", "??"}[c.state]
+	case 0x07:
+		return []string{"??", "??", "??", "select_known_packs", "??"}[c.state]
 	}
 
-	if id > len(res) {
-		return "??"
-	}
-
-	return res[id][c.state]
+	return "??"
 }
 
 func router(c *client, id int, data []byte) error {
-	c.logger.Debug(fmt.Sprintf("Current state: %s", c.state.string()), "resource", resName(c, id))
+	c.logger.Debug("", "state", c.state.string(), "protocol", fmt.Sprintf("0x%02x", id), "resource", resName(c, id))
 
 	switch id {
-	case 0x0:
+	case 0x00:
 		return protocol0(c, data)
-	case 0x1:
+	case 0x01:
 		return protocol1(c, data)
-	case 0x2:
+	case 0x02:
 		return protocol2(c, data)
-	case 0x3:
+	case 0x03:
 		return protocol3(c, data)
+	case 0x07:
+		return protocol7(c, data)
 	default:
 		return fmt.Errorf("Unknown protcol %d", id)
 	}
+}
+
+func protocol0(c *client, data []byte) error {
+	switch c.state {
+	case Handshaking:
+		// intention
+		c.logger.Debug("", "buffer", data)
+
+		m, err := readFromBuffer(data,
+			factoryPair{"protocol", intFactory},
+			factoryPair{"host", bytesFactory},
+			factoryPair{"port", ushortFactory},
+			factoryPair{"intent", intFactory},
+		)
+
+		if err != nil {
+			return err
+		}
+
+		c.logger.Debug("", "protocol", m["protocol"].(int))
+		c.logger.Debug("", "host", string(m["host"].([]byte)))
+		c.logger.Debug("", "port", m["port"].(int))
+
+		c.state = State(m["intent"].(int))
+
+		return nil
+
+	case Status:
+		// status_request
+		p := make([]player, 0)
+
+		info := &status{
+			Version: version{
+				Name:     "1.21.8",
+				Protocol: 772,
+			},
+			Players: players{
+				Max:    1,
+				Online: 0,
+				Sample: p,
+			},
+			Description: description{
+				Text: "§3✦ §cMinecraft Server in Go §3✦§7\n§eDon't forget to leave a star on Github",
+			},
+			Favicon:            AQUA,
+			EnforcesSecureChat: false,
+		}
+
+		jdata, err := json.Marshal(info)
+		if err != nil {
+			return err
+		}
+
+		// status_response
+		if err = c.send(0x00, jdata); err != nil {
+			return err
+		}
+
+	case Login:
+		// hello
+		m, err := readFromBuffer(data,
+			factoryPair{"username", bytesFactory},
+			factoryPair{"uuid", uuidFactory},
+		)
+
+		if err != nil {
+			return err
+		}
+
+		username := string(m["username"].([]byte))
+		id := m["uuid"].(uuid.UUID)
+
+		c.register(username, id)
+
+		key, err := c.publicKey()
+		if err != nil {
+			return err
+		}
+
+		c.logger.Debug("", "username", username)
+		c.logger.Debug("", "uuid", id)
+
+		if err = c.send(0x01, "", key, c.rng, true); err != nil {
+			return err
+		}
+
+	case Config:
+		// client_information
+		m, err := readFromBuffer(data,
+			factoryPair{"locale", bytesFactory},
+			factoryPair{"view_distance", byteFactory},
+			factoryPair{"chat_mode", intFactory},
+			factoryPair{"chat_color", byteFactory},
+			factoryPair{"skin_part", byteFactory},
+			factoryPair{"main_hand", intFactory},
+			factoryPair{"filter_text", byteFactory},
+			factoryPair{"allow_listing", byteFactory},
+			factoryPair{"particul_status", intFactory},
+		)
+
+		if err != nil {
+			return err
+		}
+
+		c.info.cfg = userConfig{
+			locale:       string(m["locale"].([]byte)),
+			viewDistance: int8(m["view_distance"].(byte)),
+			chat:         chatMode(m["chat_mode"].(int)),
+			chatColors:   m["chat_color"].(byte) == 0x01,
+			skinPart:     uint8(m["skin_part"].(byte)),
+			isHandLeft:   m["main_hand"].(int) == 0x00,
+			textFiltered: m["filter_text"].(byte) == 0x01,
+			allowListing: m["allow_listing"].(byte) == 0x01,
+			particul:     particulStatus(m["particul_status"].(int)),
+		}
+
+		c.logger.Debug("",
+			"locale", c.info.cfg.locale,
+			"viewDistance", c.info.cfg.viewDistance,
+			"chatMode", c.info.cfg.chat.string(),
+			"chatColors", c.info.cfg.chatColors,
+			"skinPart", c.info.cfg.skinPart,
+			"isHandLeft", c.info.cfg.isHandLeft,
+			"textFiltered", c.info.cfg.textFiltered,
+			"allowListing", c.info.cfg.allowListing,
+			"particul", c.info.cfg.particul.string(),
+		)
+
+		// custom_payload
+		if err = c.send(0x01, "minecraft:brand", "vanilla"); err != nil {
+			return err
+		}
+
+		// select_known_packs
+		if err = c.send(0x0e, 1, "minecraft", "core", "1.21.8"); err != nil {
+			return err
+		}
+
+	default:
+		return fmt.Errorf("State not handled %s", c.state.string())
+	}
+
+	return nil
 }
 
 func protocol1(c *client, data []byte) error {
 	switch c.state {
 	case Status:
 		// ping_request
-
 		timestamp := int64(binary.BigEndian.Uint64(data))
 		c.logger.Debug("", "timestamp", timestamp)
-		c.send(0x01, timestamp)
+
+		// pong_response
+		if err := c.send(0x01, timestamp); err != nil {
+			return err
+		}
 
 	case Login:
 		// key
-
 		m, err := readFromBuffer(data,
 			factoryPair{"secret", bytesFactory},
 			factoryPair{"token", bytesFactory},
@@ -102,98 +253,13 @@ func protocol1(c *client, data []byte) error {
 		c.logger.Debug("", "secret", secret)
 		c.logger.Debug("", "token", plain)
 
-		c.send(0x02, c.info.uuid, c.info.name, []byte{})
+		// login_finished
+		if err = c.send(0x02, c.info.uuid, c.info.name, []byte{}); err != nil {
+			return err
+		}
 
 	default:
-		return fmt.Errorf("State not handled %v", c.state)
-	}
-
-	return nil
-}
-
-func protocol0(c *client, data []byte) error {
-	switch c.state {
-	case Handshaking:
-		// intention
-
-		c.logger.Debug("", "buffer", data)
-
-		m, err := readFromBuffer(data,
-			factoryPair{"protocol", intFactory},
-			factoryPair{"host", bytesFactory},
-			factoryPair{"port", ushortFactory},
-			factoryPair{"intent", intFactory},
-		)
-
-		if err != nil {
-			return err
-		}
-
-		c.logger.Debug("", "protocol", m["protocol"].(int))
-		c.logger.Debug("", "host", string(m["host"].([]byte)))
-		c.logger.Debug("", "port", m["port"].(int))
-
-		c.state = State(m["intent"].(int))
-
-		return nil
-
-	case Status:
-		// status_request
-
-		p := make([]player, 0)
-
-		info := &status{
-			Version: version{
-				Name:     "1.21.8",
-				Protocol: 772,
-			},
-			Players: players{
-				Max:    1,
-				Online: 0,
-				Sample: p,
-			},
-			Description: description{
-				Text: "§3✦ §cMinecraft Server in Go §3✦§7\n§eDon't forget to leave a star on Github",
-			},
-			Favicon:            AQUA,
-			EnforcesSecureChat: false,
-		}
-
-		jdata, err := json.Marshal(info)
-		if err != nil {
-			return err
-		}
-
-		c.send(0x00, jdata)
-
-	case Login:
-		// hello
-		m, err := readFromBuffer(data,
-			factoryPair{"username", bytesFactory},
-			factoryPair{"uuid", uuidFactory},
-		)
-
-		if err != nil {
-			return err
-		}
-
-		username := string(m["username"].([]byte))
-		id := m["uuid"].(uuid.UUID)
-
-		c.register(username, id)
-
-		key, err := c.publicKey()
-		if err != nil {
-			return err
-		}
-
-		c.logger.Debug("", "username", username)
-		c.logger.Debug("", "uuid", id)
-
-		c.send(0x01, "", key, c.rng, true)
-
-	default:
-		return fmt.Errorf("State not handled %v", c.state)
+		return fmt.Errorf("State not handled %s", c.state.string())
 	}
 
 	return nil
@@ -227,8 +293,51 @@ func protocol3(c *client, data []byte) error {
 	case Login:
 		// login_acknowledged
 		c.state = Config
+	case Config:
+		// finish_configuration
+		c.state = Play
+
+		// login
+		c.send(
+			0x2b, int32(c.id), false, []string{"minecraft", "overworld"}, 1, 10, 10, false, false, false, 0, "minecraft:overworld", 69, byte(1), byte(0), false, true, false,
+			10, 0, false,
+		)
 	default:
-		return fmt.Errorf("State not handled %v", c.state)
+		return fmt.Errorf("State not handled %s", c.state.string())
 	}
+	return nil
+}
+
+func protocol7(c *client, data []byte) error {
+	switch c.state {
+	case Config:
+		// select_known_packs
+		length, sz, err := readVarIntFromBuff(data)
+		if err != nil {
+			return err
+		}
+
+		data = data[sz:]
+
+		for i := 0; i < length; i++ {
+			m, err := readFromBuffer(data,
+				factoryPair{"namespace", bytesFactory},
+				factoryPair{"id", bytesFactory},
+				factoryPair{"version", bytesFactory},
+			)
+
+			if err != nil {
+				return err
+			}
+
+			c.logger.Debug("Available pack", "namespace", string(m["namespace"].([]byte)), "id", string(m["id"].([]byte)), "version", string(m["version"].([]byte)))
+		}
+
+		c.send(0x03)
+
+	default:
+		return fmt.Errorf("State not handled %s", c.state.string())
+	}
+
 	return nil
 }
